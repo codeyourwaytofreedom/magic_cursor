3988e85e92b436c668c73d756b30d652c898aa7f
codeyourwaytofreedom <can.kolej@gmail.com>
2023-06-25 16:05:32 +0300

    Relative portioning in place.
    Grid according to cursor position.
    
    Now will apply it to background shade.
    
    Keep going.
    
    Remember why you started.

--- components/homie.tsx
+++ components/homie.tsx
@@ -1,7 +1,56 @@
+import React, { useEffect } from "react";
 import h from "../styles/Homie.module.css";
 
 
 const Homie = () => {
+    useEffect(() => {
+/*         const {innerWidth, innerHeight} = window;
+        console.log(innerWidth,innerHeight) */
+
+        const moving = (event:MouseEvent) => {
+        const {innerWidth, innerHeight} = window;
+          if(event.clientX < innerWidth/3){
+             if(event.clientY < innerHeight/3){
+                console.log("left top")
+             }
+             else if(event.clientY > innerHeight*2/3){
+                console.log("left bottom")
+             }
+             else{
+                console.log("left")
+             }
+          }
+          else if(event.clientX > innerWidth*2/3){
+            if(event.clientY < innerHeight/3){
+                console.log("right top")
+             }
+             else if(event.clientY > innerHeight*2/3){
+                console.log("right bottom")
+             }
+             else{
+                console.log("right")
+             }
+         }
+         else{
+            if(event.clientY < innerHeight/3){
+                console.log("center top")
+             }
+             else if(event.clientY > innerHeight*2/3){
+                console.log("center bottom")
+             }
+             else{
+                console.log("center center")
+             }
+         }
+        };
+      
+        window.addEventListener("mousemove", moving);
+      
+        return () => {
+          window.removeEventListener("mousemove", moving);
+        };
+      }, []);
+
     return ( <>
         <div className={h.homie}>
             Magic starts
