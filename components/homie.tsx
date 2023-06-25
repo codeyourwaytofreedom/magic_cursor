e33a9d6d8d1bb65366d19ee2ed4ecdb6e2b212ee
codeyourwaytofreedom <can.kolej@gmail.com>
2023-06-25 18:47:42 +0300

    Dynamic id in place.
    Dynamic shade on the page..
    following mouse cursor.
    
    Attention-grabbing detail...
    
    Keep going.
    
    Remember why you started.

--- components/homie.tsx
+++ components/homie.tsx
@@ -1,42 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import h from "../styles/Homie.module.css";
 
 
 const Homie = () => {
+    const [shade, setShade] = useState<string>("centercenter");
     useEffect(() => {
         const moving = (event:MouseEvent) => {
         const {innerWidth, innerHeight} = window;
           if(event.clientX < innerWidth/3){
              if(event.clientY < innerHeight/3){
-                console.log("left top")
+                console.log("left top");
+                setShade("lefttop");
              }
              else if(event.clientY > innerHeight*2/3){
-                console.log("left bottom")
+                console.log("left bottom");
+                setShade("leftbottom");
              }
              else{
-                console.log("left")
+                console.log("left");
+                setShade("leftleft");
              }
           }
           else if(event.clientX > innerWidth*2/3){
             if(event.clientY < innerHeight/3){
-                console.log("right top")
+                console.log("right top");
+                setShade("righttop");
              }
              else if(event.clientY > innerHeight*2/3){
-                console.log("right bottom")
+                console.log("right bottom");
+                setShade("rightbottom");
              }
              else{
-                console.log("right")
+                console.log("right");
+                setShade("rightright");
              }
          }
          else{
             if(event.clientY < innerHeight/3){
-                console.log("center top")
+                console.log("center top");
+                setShade("centertop");
              }
              else if(event.clientY > innerHeight*2/3){
-                console.log("center bottom")
+                console.log("center bottom");
+                setShade("centerbottom");
              }
              else{
-                console.log("center center")
+                console.log("center center");
+                setShade("centercenter");
              }
          }
         };
@@ -49,7 +59,7 @@ const Homie = () => {
       }, []);
 
     return ( <>
-        <div className={h.homie} id={h.centercenter}>
+        <div className={h.homie} id={h[`${shade}`]}>
             Magic starts
         </div>
                 
