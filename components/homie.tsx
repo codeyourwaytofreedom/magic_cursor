c3d2301f1538abea0439218303edb92bdb0af489
codeyourwaytofreedom <can.kolej@gmail.com>
2023-07-01 16:16:38 +0300

    money bags added.
    Relative position made dynamic.
    
    Keep going.
    
    Remember why you started.

--- components/homie.tsx
+++ components/homie.tsx
@@ -10,9 +10,12 @@ const Homie = () => {
     useEffect(() => {
         const moving = (event:MouseEvent) => {
         const {innerWidth, innerHeight} = window;
-        const height_percentage = event.clientY < innerHeight/2 ? event.clientY/innerHeight/2 * -80 
-                                  : -(20 + (20 - event.clientY/innerHeight/2 * 80));
-        setSlope(height_percentage);
+        const height_percentage = event.clientX < innerWidth/2 ? event.clientX/innerWidth/2 * -80 
+                                  : -(20 + 2*(20 - event.clientX/innerWidth/2 * 80));
+         setTimeout(() => {
+            setSlope(height_percentage);
+         }, 350);
+        
 
         console.log(height_percentage);
 
@@ -70,13 +73,16 @@ const Homie = () => {
     return ( <>
         <div className={h.homie} id={h[`${shade}`]}>
             <div className={h.homie_scales}>
-                <Image id={h.holders} style={{ transform: `rotate(${slope}deg)` }}  src={"/holders.png"} alt={"scale"} width={600} height={400}/>
+               <div className={h.homie_scales_double} style={{ transform: `rotate(${slope}deg)` }}>
+                  <Image id={h.holders}  src={"/holders.png"} alt={"scale"} width={600} height={400}/>
+                  <Image id={h.moneyleft} style={{left:slope/2+10}}  src={"/money.png"} alt={"scale"} width={50} height={50}/>
+                  <Image id={h.moneyright} style={{right:-slope/2+10}}  src={"/money.png"} alt={"scale"} width={50} height={50}/>
+               </div>
                 <Image id={h.center}  src={"/center.png"} alt={"scale"} width={600} height={400}/>
             </div>
             <div className={h.homie_navbar}>
                 
             </div>
-            <h1>{slope}</h1>
         </div>
                 
     </> );
