f350bb2635ef812d09de73a93fcfb4d933ef08ad
codeyourwaytofreedom <can.kolej@gmail.com>
2023-07-02 18:30:47 +0300

    Data bank under construction.
    Scrape function applied to data bank..
    
    Successful testing.
    
    Great step forward.
    
    Keep going.
    
    Remmeber why you started.

--- components/product_bank.js
+++ components/product_bank.js
@@ -2,3 +2,20 @@ export const urls_austria = [
     "https://www.interspar.at/shop/lebensmittel/spar-cola/p/7114274",
     "https://www.interspar.at/shop/lebensmittel/pepsi-cola-cola/p/2020004406385"
 ]
+
+export const urls ={
+    drinks:{
+        categoryName:"Drinks",
+        products:[
+            {
+                product_name:"Pepsi - Cola 330 ml",
+                product_urls:[
+                    {
+                        country:"Austria",
+                        link:"https://www.interspar.at/shop/lebensmittel/pepsi-cola-cola/p/2020004406385"
+                    }
+                ]
+            }
+        ]
+    }
+}
--- pages/api/hm.ts
+++ pages/api/hm.ts
@@ -4,9 +4,14 @@ import puppeteer from "puppeteer-extra";
 import { executablePath } from 'puppeteer';
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import {urls_austria} from "../../components/product_bank";
+import {urls} from "../../components/product_bank";
 
 type Data = {
   name: string
+}
+
+type url_interface = {
+  url:string
 }
 
 
@@ -17,14 +22,14 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   
-  async function fetch_price() {
+  async function fetch_price(url) {
     try {
       const browser = await puppeteer.launch({
         headless: "new",
         executablePath: executablePath()
       });
       const page = await browser.newPage();
-      await page.goto(urls_austria[1]);
+      await page.goto(url);
       await page.waitForSelector('label.productDetailsPrice');
       await page.waitForSelector('h1.productDetailsName');
   
@@ -42,8 +47,8 @@ export default async function handler(
     }
   }
 
-fetch_price();
+urls.drinks.products.forEach((pro) => pro.product_urls.map((p_url) => fetch_price(p_url.link)))
 
-  res.status(200).json({ name: 'John Doe' });
+res.status(200).json({ name: 'John Doe' });
 
 }
