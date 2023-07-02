f361aaa279dd2f3ef5a5a628b99564a875f59b17
codeyourwaytofreedom <can.kolej@gmail.com>
2023-07-02 14:15:21 +0300

    Price successfully scrape.
    Next step: create a product object and
    scrape all relevant info.
    
    Keep going.
    
    Remember why you started.

--- pages/api/hm.ts
+++ pages/api/hm.ts
@@ -1,21 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-const puppeteer = require("puppeteer-extra");
-const {executablePath} = require('puppeteer')
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+import puppeteer from "puppeteer-extra";
+import { executablePath } from 'puppeteer';
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
 
 type Data = {
   name: string
 }
-function wait(ms:any) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
 
-async function waitForTimeout(milliseconds:any) {
-  await wait(milliseconds);
-  console.log('Waited for', milliseconds, 'milliseconds.');
-}
+
 puppeteer.use(StealthPlugin());
 
 export default async function handler(
@@ -23,34 +17,31 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   
-  async function run(){
-    const browser = await puppeteer.launch({
-        headless:true,
+  async function fetch_price() {
+    try {
+      const browser = await puppeteer.launch({
+        headless: "new",
         executablePath: executablePath()
-    })
-    const page = await browser.newPage()
-    
-    await page.goto("https://www.youtube.com/watch?v=CKThDnCf2C8");
-    await page.waitForTimeout(5000);    
-
-    for (let index = 0; index < 50; index++) {
-        await page.keyboard.press("PageDown");
-        await page.waitForTimeout(500);
+      });
+      const page = await browser.newPage();
+  
+      await page.goto("https://www.interspar.at/shop/lebensmittel/pepsi-cola-cola/p/2020004406385");
+      await page.waitForSelector('label.productDetailsPrice');
+  
+      const value = await page.evaluate(() => {
+        const label = document.querySelector('label.productDetailsPrice');
+        return label?.textContent;
+      });
+      console.log(value);
+  
+      await browser.close();
+    } catch (error) {
+      console.error("An error occurred:", error);
     }
+  }
 
+fetch_price();
 
-    const comments = await page.$$("#content-text");
-    console.log("Length is: ",comments.length)
-    let fin = []
-    for (let i = 0; i < comments.length; i++) {
-            const element = comments[i];
-            const element_text = await page.evaluate(element => element.textContent, element)
-            console.log(element_text);
-            fin.push({"comment":element_text})
-        }           
-    browser.close()
-}
-run();
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' });
 
 }
