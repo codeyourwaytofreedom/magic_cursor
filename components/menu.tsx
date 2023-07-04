b65711e038104fab6623f88c33c0cbc006a7d3d0
codeyourwaytofreedom <can.kolej@gmail.com>
2023-07-04 22:03:11 +0300

    Second slider to be added...
    Product menu under construction.
    
    Keep going.
    
    Remember why you started.

--- components/menu.tsx
+++ components/menu.tsx
@@ -5,13 +5,24 @@ const Menu = () => {
     return ( <>
             <div className={h.homie_menu}>
                 <div className={h.homie_menu_clicks}>
+                    {
+                        [...Array(6)].map((e,i)=>
+                        <>
+                                            <button>
+                        <Image src={"/flat.png"} alt={"oil"} width={70} height={90}/>
+                        <div id={h.text}>Flat</div>
+                    </button>
                     <button>
                         <Image src={"/oil.png"} alt={"oil"} width={70} height={90}/>
+                        <div id={h.text}>Oil</div>
                     </button>
                     <button>
                         <Image src={"/milk.png"} alt={"oil"} width={70} height={90}/>
-                        <div id={h.text}>Oil</div>
+                        <div id={h.text}>Milk</div>
                     </button>
+                        </>
+                        )
+                    }
                 </div>
             </div>
     </> );
