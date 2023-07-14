df408553f8ac90682dae194436a1995c7323f88d
codeyourwaytofreedom <can.kolej@gmail.com>
2023-07-14 15:43:21 +0300

    Mapping page under construction.
    Canvas drawing in place.
    
    Tested with dummy coordinates.
    
    Next step: use docs data to populate Canvas.
    
    Keep going.
    
    Remmeber why you started.

--- pages/mapping.tsx
+++ pages/mapping.tsx
@@ -1,13 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import m from "../styles/Mapping.module.css";
 
 const Mapping = () => {
     
     const [docs, setDocs] = useState<[]>();
+    const canvas = useRef<HTMLCanvasElement>(null);
 
 
     useEffect(()=>{
-       fetch("/api/map").then(r=>r.json()).then(rs => setDocs(rs))
-    },[])
+       fetch("/api/map").then(r=>r.json()).then(rs => {
+        setDocs(rs);
+        rs.map((r:any)=> console.log(r.route.wander))
+       } )
+    },[]);
+
+    useEffect(()=>{
+       const ctx = canvas.current?.getContext('2d')!;
+       const startX = 50; // X-coordinate of the start point
+       const startY = 50; // Y-coordinate of the start point
+       const endX = 350; // X-coordinate of the end point
+       const endY = 350; // Y-coordinate of the end point
+   
+       // Draw the line
+       ctx.beginPath();
+       ctx.moveTo(startX, startY);
+       ctx.lineTo(endX, endY);
+       ctx.stroke();
+    },[docs])
 
 
     
@@ -17,6 +36,12 @@ const Mapping = () => {
             {
                 docs && docs.length
             }
+            <div className={m.shell}>
+                <canvas ref={canvas}>
+
+                </canvas>
+            </div>
+             
         </>
      );
 }
